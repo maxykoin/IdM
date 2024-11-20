9ffd0fd858a08a84dd5dff347bdfad855cd8c8bc
maxykoin <86720966+maxykoin@users.noreply.github.com>
2024-11-20 14:39:13 -0300

    201124 - Version 2
    - Cleaner and more compact code
    - When items are selected, only them will be changed
    - When there are no items selected, everything displayed on the table will be changed

--- idmV2.py
+++ idmV2.py
@@ -4,6 +4,7 @@ import ttkbootstrap as ttk
 import pandas as pd
 from openpyxl import load_workbook
 from datetime import datetime
+from unidecode import unidecode
 
 class App(tk.Frame):
     def __init__(self, master):
@@ -23,7 +24,7 @@ class App(tk.Frame):
             self.geral = self.planilha['Geral']
             self.perdido = self.planilha['Perdidos']
         except:
-            tk.messagebox.showerror(title='Erro', message="Base de dados não encontrada. \n  Contactar suporte.")
+            tk.messagebox.showerror(title='Erro', message="Base de dados não encontrada.")
         else:
             self.buscaDados()
     
@@ -55,7 +56,7 @@ class App(tk.Frame):
             self.entry = tk.Entry(cracha)
             self.entry.pack(pady=5)
             self.entry.focus_set()
-            self.entry.bind('<Return>', lambda event: treat)
+            self.entry.bind('<Return>', lambda event: treat())
 
             if op == 'norma':
                 label['text'] = 'Insira a Norma:'
@@ -78,10 +79,12 @@ class App(tk.Frame):
                         elif semana > 52 or semana < 1 or ano > 99 or ano < 10:
                             raise ValueError
                     elif self.op == 'norma':
-                        nome_instrumento = self.entry.get()
-                        final = self.entry.get().split('-')
-                        final = final[-1].strip()
-                        nome_instrumento = ''.join(nome_instrumento.split())
+                        self.norma = self.entry.get()
+                        self.final = self.entry.get().split('-')
+                        self.final = self.final[-1].strip()
+                        self.norma = ''.join(self.norma.split())
+                    elif self.op == 'nome':
+                        self.nome = unidecode(self.entry.get()).lower()
                     if not self.entry.get():
                         raise ValueError
                 except ValueError:
@@ -96,7 +99,7 @@ class App(tk.Frame):
         butFrame = tk.Frame(frame)
         butFrame.pack()
 
-        butNorma = tk.Button(butFrame, text="Norma", font='Calibri 12', width=10 , height=2, command=lambda: inputBox('norma'))
+        butNorma = tk.Button(butFrame, text="Norma", font='Calibri 12', width=10 , height=2, command=lambda:inputBox('norma'))
         butNorma.pack(side=tk.LEFT, padx=10)
 
         butKw = tk.Button(butFrame, text="KW", font='Calibri 12', width=10 , height=2,command=lambda:inputBox('kw'))
@@ -107,21 +110,41 @@ class App(tk.Frame):
     
     def buscaDados(self):
         dados = []
+        try:
+            for sheet in self.planilha.worksheets:
+                sheet = self.planilha[sheet.title]
+                columns = [cell.value for cell in next(sheet.iter_rows())]
+                for row in sheet.iter_rows():
+                    for i in range(len(row)):
+                        if row[i].value in [self.entry.get()]:
+                            item = [cell.value for cell in row]
+                            if not any(item[:3] == data[:3] for data in dados):
+                                dados.append(item)
+                                self.sheet = sheet.title
+                        if self.op == 'nome' and i == 2:
+                            instrumento = unidecode(str(row[i].value)).lower()
+                            if self.nome in instrumento.split():
+                                item = [cell.value for cell in row]
+                                if not any(item[:3] == data[:3] for data in dados):
+                                    dados.append(item)
+                                    self.sheet = sheet.title
+                        if self.op == 'norma' and i == 4:
+                            instrumento = str(row[i].value)
+                            instrumento = instrumento.strip()
+                            final = instrumento.split('-')
+                            final = final[-1].strip()
+                            instrumento = ''.join(instrumento.split())
+                            if final in [self.final] or instrumento in [self.norma]:
+                                item = [cell.value for cell in row]
+                                if not any(item[:3] == data[:3] for data in dados):
+                                    dados.append(item)
+                                    self.sheet = sheet.title
 
-        for sheet in self.planilha.worksheets:
-            sheet = self.planilha[sheet.title]
-            columns = [cell.value for cell in next(sheet.iter_rows())]
-            for row in sheet.iter_rows():
-                for i in range(len(row)):
-                    if row[i].value in [self.entry.get()]:
-                        item = [cell.value for cell in row]
-                        if not any(item[:3] == data[:3] for data in dados):
-                            dados.append(item)
-                            self.sheet = sheet.title
-
-        self.df = pd.DataFrame(dados, columns=columns)
-
-        self.mostraDados()
+            self.df = pd.DataFrame(dados, columns=columns)
+        except:
+            tk.messagebox.showerror(title='Erro', message="Valor não encontrado.")
+        else:
+            self.mostraDados()
 
     def mostraDados(self):
         self.limpar()
@@ -156,6 +179,45 @@ class App(tk.Frame):
             item = self.table.item(self.table.selection())
             if item['values'] in self.selecionado:
                 self.selecionado.remove(item['values'])
+        
+        def novoKw():
+            cracha = tk.Toplevel(root)
+
+            width = self.master.winfo_screenwidth()
+            height = self.master.winfo_screenheight()
+            left = int(width / 2 - 200 / 2)
+            top = int(height / 2 - 200 /2)
+
+            cracha.geometry(f'200x200+{left}+{top}')
+
+            label = tk.Label(cracha, text='Insira um novo kw', font = 'Calibri 18 bold')
+            label.pack(pady=5)
+
+            self.novokw = tk.Entry(cracha)
+            self.novokw.pack(pady=5)
+            self.novokw.focus_set()
+            self.novokw.bind('<Return>', lambda event: self.mudaKw())
+
+            butBusca = tk.Button(cracha, text="Pesquisar", font='Calibri 12', width=10 , height=1, command=self.mudaKw)
+            butBusca.pack(pady=15)
+
+        def novaRetirada():
+            try:
+                if len(self.selecionado) == 0:
+                    for row in self.planilha[self.sheet].iter_rows():
+                        if any(row[4].value == item[4] for item in self.df.values):
+                            hoje = datetime.now()
+                            row[5].value = hoje.strftime("%d-%m-%Y %H:%M:%S")
+                else:
+                    for row in self.planilha[self.sheet].iter_rows():
+                        if any(row[4].value == selected[4] for selected in self.selecionado):
+                            hoje = datetime.now()
+                            row[5].value = hoje.strftime("%d-%m-%Y %H:%M:%S")
+
+                self.planilha.save('Instrumentos de medição.xlsx')
+                tk.messagebox.showinfo(title='Sucesso', message="Valor modificado com sucesso.")
+            except:
+                tk.messagebox.showerror(title='Erro', message='Erro')
 
         self.table.bind('<<TreeviewSelect>>', select)
         self.table.bind('<Double-1>', delete)
@@ -163,12 +225,39 @@ class App(tk.Frame):
         butFrame = tk.Frame()
         butFrame.pack(side=tk.BOTTOM)
 
-        butKw = tk.Button(butFrame, text="Mudar KW", font = 'Calibri 12', width=15, height=1)
+        butKw = tk.Button(butFrame, text="Mudar KW", font = 'Calibri 12', width=15, height=1, command=lambda: novoKw())
         butKw.pack(side=tk.LEFT, pady=10)
         
-        butRetirada = tk.Button(butFrame, text="Mudar Retirada", font = 'Calibri 12', width=15, height=1)
+        butRetirada = tk.Button(butFrame, text="Mudar Retirada", font = 'Calibri 12', width=15, height=1, command=lambda: novaRetirada())
         butRetirada.pack(padx=25, pady=10)
 
+    def mudaKw(self):
+        try:
+            kw = self.novokw.get().split('/')
+            semana = int(kw[0])
+            ano = int(kw[1])
+
+            if len(kw) != 2 or semana < 1 or semana > 52 or ano < 10 or ano > 99:
+                raise ValueError("Invalid KW format.")
+
+            if len(self.selecionado) == 0:
+                for row in self.planilha[self.sheet].iter_rows():
+                    if any(row[4].value == item[4] for item in self.df.values):
+                        row[7].value = self.novokw.get()
+                        hoje = datetime.now()
+                        row[6].value = hoje.strftime("%d-%m-%Y %H:%M:%S")
+            else:
+                for row in self.planilha[self.sheet].iter_rows():
+                    if any(row[4].value == selected[4] for selected in self.selecionado):
+                        row[7].value = self.novokw.get()
+                        hoje = datetime.now()
+                        row[6].value = hoje.strftime("%d-%m-%Y %H:%M:%S")
+
+            self.planilha.save('Instrumentos de medição.xlsx')
+            tk.messagebox.showinfo(title='Sucesso', message="Valor modificado com sucesso.")
+        
+        except ValueError as ve:
+            tk.messagebox.showerror(title='Erro', message=str(ve))
 
 root = ttk.Window(themename ='pulse')
 app = App(root)
